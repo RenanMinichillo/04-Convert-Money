66e4ed67640c11dd2e9ef4fcc6d34008b43995c9
Renan Minichillo <140018183+RenanMinichillo@users.noreply.github.com>
2025-04-11 10:51:54 -0300

    atualizado script e implementado consumo de api

--- script.js
+++ script.js
@@ -1,36 +1,60 @@
-const convertButton = document.querySelector('#convert-button');
+const convertButton = document.querySelector('#convert-button')
 
-const rates = {
-    real: { locales: 'pt-BR', currency: 'BRL', name: 'R$ Real', flag: 'img/real.png', bid: 1 },
-    dolar: { locales: 'en-US', currency: 'USD', name: 'US$ Dólar', flag: 'img/dolar.png', bid: 5.91 },
-    euro: { locales: 'de-DE', currency: 'EUR', name: '€ Euro', flag: 'img/euro.png', bid: 6.2 },
-    libra: { locales: 'en-GB', currency: 'GBP', name: '£ Libra', flag: 'img/libra.png', bid: 7.3 },
-    iene: { locales: 'ja-JP', currency: 'JPY', name: '¥ Iene', flag: 'img/iene.png', bid: 0.04 },
-    won: { locales: 'ko-KR', currency: 'KRW', name: '₩ Won', flag: 'img/won.png', bid: 0.0041 },
-    bitcoin: { locales: 'en-US', currency: 'BTC', name: 'BTC Bitcoin', flag: 'img/bitcoin.png', bid: 607.040 }
-};
+function formatValues(locales, currency, value) {
+    const formatedValue = new Intl.NumberFormat(locales, {
+        style: 'currency',
+        currency: currency
+    }).format(value)
 
-function convertValues() {
-    const amountInput = parseFloat(document.querySelector('#amount-input').value);
-    const fromCurrency = document.querySelector('#currency-select-from').value;
-    const toCurrency = document.querySelector('#currency-select-to').value;
-
-    const selectedFrom = rates[fromCurrency];
-    const selectedTo = rates[toCurrency];
-
-    const convertedValue = (amountInput * selectedFrom.bid) / selectedTo.bid;
-
-    updateUI('.flag.convert', '#currency-name-from', '#currency-value-from', selectedFrom, amountInput);
-    updateUI('.flag.converted', '#currency-name-to', '#currency-value-to', selectedTo, convertedValue);
+    return formatedValue
 }
 
-function updateUI(flagSelector, nameSelector, valueSelector, currency, value) {
+async function convertValues() {
+    const selectFrom = document.querySelector('#convert-from')
+    const selectTo = document.querySelector('#convert-to')
+    const currencyValue = parseFloat(document.querySelector('#currency-value').value)
+    const infoQuotes = document.querySelector('.rates')
+
+    const quotes = 'USD-BRL,EUR-BRL,BTC-BRL,JPY-BRL,GBP-BRL'
+    const url = `https://economia.awesomeapi.com.br/json/last/${quotes}`
+
+    let options
+
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            throw new Error('Erro ao buscar a cotação')
+        }
+        const data = await response.json()
+
+        options = {
+            real: { locales: 'pt-BR', currency: 'BRL', name: 'Real Brasileiro', flag: 'assets/img/real.png', bid: 1 },
+            dolar: { locales: 'en-US', currency: data.USDBRL.code, name: 'Dolár Americano', flag: 'assets/img/dolar.png', bid: data.USDBRL.bid },
+            euro: { locales: 'de-DE', currency: data.EURBRL.code, name: 'Euro', flag: 'assets/img/euro.png', bid: data.EURBRL.bid },
+            iene: { locales: 'en-GB', currency: data.GBPBRL.code, name: 'Iene Japonês', flag: 'assets/img/iene.png', bid: data.JPYBRL.bid },
+            libra: { locales: 'ja-JP', currency: data.JPYBRL.code, name: 'Libra Esterlina', flag: 'assets/img/libra.png', bid: data.GBPBRL.bid },
+            bitcoin: { locales: 'en-US', currency: data.BTCBRL.code, name: 'Bitcoin', flag: 'assets/img/bitcoin.png', bid: data.BTCBRL.bid }
+        }
+
+        infoQuotes.textContent = `A cotação atual do ${options[selectTo.value].name} é ${options[selectTo.value].bid}`
+
+    } catch (err) {
+        console.error('Erro:', err)
+    }
+
+    const currencySelectedFrom = options[selectFrom.value]
+    const currencySelectedTo = options[selectTo.value]
+
+    const convertedValue = (currencyValue * currencySelectedFrom.bid) / currencySelectedTo.bid
+
+    updateInfo('.image-convert', '.name-convert', '.input-value', currencySelectedFrom, currencyValue);
+    updateInfo('.image-converted', '.name-converted', '.output-value', currencySelectedTo, convertedValue);
+}
+
+function updateInfo(flagSelector, nameSelector, valueSelector, currency, value) {
     document.querySelector(flagSelector).src = currency.flag;
     document.querySelector(nameSelector).textContent = currency.name;
-    document.querySelector(valueSelector).textContent = new Intl.NumberFormat(currency.locales, {
-        style: 'currency',
-        currency: currency.currency
-    }).format(value);
+    document.querySelector(valueSelector).textContent = formatValues(currency.locales, currency.currency, value)
 }
 
-convertButton.addEventListener('click', convertValues);
+convertButton.addEventListener('click', convertValues)
